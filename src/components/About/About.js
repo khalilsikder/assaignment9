6613b3fe729daa370eab7e773e51861df4e71cf0
khalilsikder <khalil.sikder03@gmail.com>
2022-06-26 15:11:40 +0600

    change content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,9 +3,11 @@ import './About.css'
 
 const About = () => {
     return (
-        <div className='about'>
-          <p>It's a outlat of various laptop and destop.anybody can choose his/her best<br/>
-            ou main showroom at Mirpur in the capital of Dhaka.we have more then hundred of small showroom in out of Dhaka.Chittagon,Rajshahi,Borisal,Khulna has big showroom </p>  
+        <div className='about-sec'>
+          <p>
+            It's a outlat of various laptop and destop.Anybody can choose his/her best
+            our main showroom at Mirpur in the capital of Dhaka.<br/>We have more then hundred of small showroom in out of Dhaka.Chittagon,Rajshahi,Borisal,Khulna has big showroom.
+          </p>  
         </div>
     );
 };
